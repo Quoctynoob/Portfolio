b59017fab710e972523bb36dc90ae76926a41597
unknown <tilburgquoc34@gmail.com>
2024-08-14 14:19:01 +0300

    Doing navbar

--- portfolio_lvl2/app/components/navBar.tsx
+++ portfolio_lvl2/app/components/navBar.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+import AboutPage from "@/pages/about";
+import IndexPage from "@/pages";
+import ProjectsPage from "@/pages/projects";
+import ContactPage from "@/pages/contact";
 
 const NavBar = () => {
+    const [view, setView] = useState<string>('home');
+
+
+    const renderContent = () => {
+        switch (view) {
+            case 'about':
+                return <AboutPage/>
+            case 'projects':
+                return 
+        }
+    }
     return (
         <div>
-            
+            <nav>
+
+            </nav>
         </div>
     )
 }
+
+export default NavBar;
