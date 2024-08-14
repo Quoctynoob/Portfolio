a4c6cb3b40b6cffe7acca54f1a38e4a0a4af430b
unknown <tilburgquoc34@gmail.com>
2024-08-14 18:12:21 +0300

    Typewriter doen

--- portfolio_lvl2/app/components/tabs/home.tsx
+++ portfolio_lvl2/app/components/tabs/home.tsx
@@ -1,6 +1,9 @@
+import Typewriter from "../utilities/typeWriter";
+
 const HomePage = () => {
     return (
         <div>
+            <Typewriter/>
         </div>
     )
 }
---
+++ portfolio_lvl2/app/components/utilities/typeWriter.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+
+const Typewriter: React.FC = () => {
+    // Create a ref for the DOM element that Typed.js will attach to
+    const typedElementRef = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            strings: [
+                'Hello', 'Xin chào', 'Bonjour','Goedendag','こんにちは', '안녕하세요', 'привет',
+                'สวัสดี', '你好', 'Cześć','مرحبا','নমস্কার','Здравейте','Hyvää päivää',
+                'γεια σας','नमस्ते','שלום','नमस्ते','سلام','Grüß Gott'
+            ],
+            typeSpeed: 100,
+            backSpeed: 50,
+            loop: true,
+        };
+
+        // Create a Typed instance and pass it the element and options
+        const typed = new Typed(typedElementRef.current, options);
+
+        // Cleanup function to destroy the Typed instance on component unmount
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
+    return (
+        <div>
+            {/* Element that will display the typewriter effect */}
+            <span id="element" ref={typedElementRef}></span>
+        </div>
+    );
+};
+
+export default Typewriter;
