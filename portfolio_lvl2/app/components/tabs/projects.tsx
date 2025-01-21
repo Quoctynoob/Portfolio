cb5ca07b9070c35eb725e6fa17827af83f98e228
Quocty <tilburgquoc34@gmail.com>
2025-01-21 03:42:42 -0500

    Updating Projects section

--- portfolio_lvl2/app/components/tabs/projects.tsx
+++ portfolio_lvl2/app/components/tabs/projects.tsx
@@ -8,7 +8,7 @@ const ProjectsPage = () => {
             <h1 className="text-5xl font-bold text-center text-green-200 mb-5">
                 Projects<span className="text-green-500">.</span>
             </h1>
-            <div className="grid grid-cols-3 gap-6 ">
+            <div className="grid grid-cols-3 gap-6">
                 <ProjectDisplay 
                     imageSrc="/images/tennisproject.jpg"
                     link="https://tennis-locator.vercel.app/"
@@ -21,32 +21,32 @@ const ProjectsPage = () => {
                     imageSrc="/images/restaurantproject.jpg"
                     link="https://quoctynoob.github.io/Me-s-restaurant/"
                     text="Online Menu"
-                    title="Tennis Locator"
-                    description=""
+                    title="Me's Restaurant"
+                    description="a simple and responsive online menu"
                 />
 
                 <ProjectDisplay
-                    imageSrc=""
+                    imageSrc="/images/ePortfolio.png"
                     link="https://github.com/Quoctynoob/Stock-Portfolio-Management"
                     text="Command Line Stock Portfolio Management"
-                    title="Tennis Locator"
-                    description=""
+                    title="Stock Portfolio Management"
+                    description="a command line stock portfolio, currently transitioning to a website"
                 />
 
                 <ProjectDisplay
                     imageSrc="/images/firebaseproject.jpg"
                     link="https://quoctynoob.github.io/LearnWithFirebase/"
                     text="Todo-list"
-                    title="Tennis Locator"
-                    description=""
+                    title="Todo-list"
+                    description="a simple website to practice with Firebase using todo-list"
                 />
 
                 <ProjectDisplay
                     imageSrc="/images/webproject.jpg"
                     link="https://quoctynoob.github.io/LearnTheWebBasics/"
                     text="Jokes and Weather API"
-                    title="Tennis Locator"
-                    description=""
+                    title="Jokes and Weather API"
+                    description="A simple website to practice with APIs"
                 />
             </div>
         </div>
